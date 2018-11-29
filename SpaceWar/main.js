cf11b654a04f3f0b87f3b3c322df633fe5c93b3a
曾宇 <zengyu@meizu.com>
2018-11-29 17:26:59 +0800

    完成玩家射击基本逻辑

--- SpaceWar/main.js
+++ SpaceWar/main.js
@@ -3,7 +3,10 @@ var ctx = canvas.getContext('2d');
 var screenWidth = canvas.width = window.innerWidth;
 var screenHeight = canvas.height = window.innerHeight;
 var smallPlaneSize = 20;
-var defaultVelocity = 10;
+var playerVelocity = 10;
+var playerBulletSize = 5;
+var playerBulletColor = 'rgb(255,140,0)';
+var playerBulletVelocity = 3;
 
 var div_main = document.getElementById('div_main');
 var div_menu = document.getElementById('div_menu');
@@ -16,7 +19,6 @@ var span_level = document.getElementById('span_level');
 var span_hp = document.getElementById('span_hp');
 var span_score = document.getElementById('span_score');
 var img_player = document.getElementById('img_player');
-var img_plane1 = document.getElementById('img_plane1');
 var minX = 0;
 var maxX = screenWidth - img_player.width;
 var minY = 0;
@@ -25,11 +27,10 @@ var maxY = screenHeight - img_player.height;
 function Player() {
     this.x = maxX / 2;
     this.y = maxY;
-    this.velocity = defaultVelocity;
+    this.velocity = playerVelocity;
 }
 
 Player.prototype.draw = function () {
-    ctx.clearRect(0, 0, screenWidth, screenHeight);
     ctx.drawImage(img_player, this.x, this.y);
 };
 
@@ -48,13 +49,60 @@ Player.prototype.move = function (dx, dy) {
     }
 };
 
+function Bullet(x, y, size, color) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+    this.velocity = playerBulletVelocity;
+    this.show = true;
+}
+
+Bullet.prototype.draw = function () {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+};
+
+Bullet.prototype.move = function (dx, dy) {
+    this.x += dx * this.velocity;
+    if (this.x < minX) {
+        this.show = false;
+    } else if (this.x > maxX) {
+        this.show = false;
+    }
+    this.y += dy * this.velocity;
+    if (this.y < minY) {
+        this.show = false;
+    } else if (this.y > maxY) {
+        this.show = false;
+    }
+};
+
 var player = new Player();
+var playerBullet = [];
 var handlerId = 0;
 var isGame = false;
 var state = false;
 
+function playerShot() {
+    var bullet = new Bullet(player.x + img_player.width / 2, player.y, playerBulletSize, playerBulletColor);
+    playerBullet.push(bullet);
+}
+
 function loop() {
+    ctx.clearRect(0, 0, screenWidth, screenHeight);
     player.draw();
+    for (var i = playerBullet.length - 1; i >= 0; i--) {
+        if (playerBullet[i].show === true) {
+            playerBullet[i].draw();
+            playerBullet[i].move(0, -1);
+        } else {
+            playerBullet.splice(i, 1);
+        }
+    }
+    console.log('length = ' + playerBullet.length);
     handlerId = requestAnimationFrame(loop);
 }
 
@@ -108,7 +156,6 @@ function attachButtonListener() {
 }
 
 window.onload = function () {
-    isLoaded = true;
     prepare();
 };
 
@@ -132,7 +179,7 @@ window.onkeydown = function (event) {
                 player.move(1, 0);
                 break;
             case 74: // J
-                // TODO
+                playerShot();
                 break;
             case 38: // up
                 // TODO
